8bbe0c234bba8effd473ca18726819546b99bb9f
Ed Opare-Aryee <ed@thisissoon.com>
2019-01-09 11:31:51 +0000

    feat(lesson-5): Intro to Javascript

---
+++ Lesson-5/README.md
@@ -0,0 +1,21 @@
+# Lesson 5 (09/01/2019) - Intro to Javascript
+
+## Topics
+
+- What is JavaScript?
+- Variables
+  - What are variables?
+  - `const`, `let` and `var`
+  - Variable types:
+    - Number
+    - String
+    - Array
+    - Object
+
+
+## Demo / Task
+
+- Create and console.log an Array of Object’s.
+- Each Object must be a person at SOON_ with their name, age, department and an Array called likes
+- likes which is just an Array of `String`s with things that person likes.
+
---
+++ Lesson-5/script.js
@@ -0,0 +1,45 @@
+// Example 1: Assigning variables
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log('Example 1', {x, y, z});
+
+// Example 2: Variable types
+
+// Number
+const age = 30;
+console.log('Example 2: Number', {age});
+
+// String
+const name = 'SOON_';
+console.log('Example 2: String', {name});
+
+// Arrays
+const arrayOfNumbers = [1, 2, 3, 4];
+const arrayOfStrings = ['a', 'b', 'c', 'd'];
+const arrayOfMixedTypes = ['a', 1, 'c', 2];
+console.log('Example 2: Array', {arrayOfStrings, arrayOfNumbers});
+
+// Objects
+const person = {
+  name: 'A Person',
+  age: 30,
+  shoppingList: ['apples', 'bananas', 'pears', 'oranges']
+};
+
+console.log('Example 2: Object', {person});
+
+
+// Example 3: Simple maths
+
+const number1 = 2;
+const number2 = 3;
+
+const additionResult = number1 + number2;
+const subtractionResult = number1 - number2;
+const divisionResult = number1 / number2;
+const multiplyResult = number1 * number2;
+console.log('Example 3: Maths', {additionResult, subtractionResult, divisionResult, multiplyResult});
+
+
