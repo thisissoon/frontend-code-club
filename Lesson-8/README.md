6eba8c01a2ebc588b0982ea0277a3c9cac40665b
Jack Matthews <jack@thisissoon.com>
2019-01-30 12:23:05 +0000

    feat(lesson-8): pseudocode and refactoring

---
+++ Lesson-8/README.md
@@ -0,0 +1,39 @@
+# Lesson 7 (23/01/2019) - If statements and more types
+
+## Topics
+
+- All the JS we've covered in previous lessons
+- Pseudocode
+- Refactoring (DRY)
+
+## Demo / Task
+
+Create a programme which figures out who is through to the next round of bakeoff while following the problem solving steps of:
+
+- identify and define problem
+- consider / plan solution (pseudocode)
+- carry out solution
+- iterate on solution (refactor)
+
+NOTE: remember to update the `script` tag in `index.html` to the file name of the step you want to view!
+
+```js
+var bakers = [
+  {
+    name: "Ken",
+    wonFirstBattle: true
+  },
+  {
+    name: "Ed",
+    wonFirstBattle: false
+  },
+  {
+    name: "Jack",
+    wonFirstBattle: true
+  },
+  {
+    name: "Tom",
+    wonFirstBattle: false
+  }
+];
+```
---
+++ Lesson-8/script-step-6.js
@@ -0,0 +1,39 @@
+// refactor and again
+
+///////////////////
+// 1. Create list of bakers with info like name and first round bake battle result
+//////////////////
+var bakers = [
+  {
+    name: "Ken",
+    wonFirstBattle: true
+  },
+  {
+    name: "Ed",
+    wonFirstBattle: false
+  },
+  {
+    name: "Jack",
+    wonFirstBattle: true
+  },
+  {
+    name: "Tom",
+    wonFirstBattle: false
+  }
+];
+/////////////////
+// 2. Create somewhere to record list of winners
+/////////////////
+/////////////////
+// 3. Look over bakers and add to winners if won in first round
+/////////////////
+// 3.1 look at baker
+// 3.2 look at first round result
+// 3.3 if they won add their name to winners list
+var winners = bakers.filter(baker => baker.wonFirstBattle);
+
+/////////////////
+// 4. Output our list of winners
+/////////////////
+console.log("Winners:");
+winners.forEach(winner => console.log(winner.name));
